abfda60d054e38eab83509248e4a6a09d7fc5c37
ktg <ktg5679@gmail.com>
2024-03-25 17:37:27 +0900

    feat: reacoilRoot설정하고 loading페이지 만듬_errorpage우선 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React, { Suspense, lazy } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Loading from "./pages/router/Loading";
 
 const LoginPage = lazy(() => import("./pages/LogIn"));
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ path: "/login", element: <LoginPage /> }],
+    children: [
+      { path: "/login", element: <LoginPage /> },
+      {
+        path: "/loading",
+        element: <Loading />,
+      },
+    ],
   },
 ]);
 
 const App = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <RouterProvider router={router} />
     </Suspense>
   );
---
+++ src/pages/router/ErrorPage.tsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <>
+      <div>This ErrorPage</div>
+    </>
+  );
+};
+
+export default ErrorPage;
